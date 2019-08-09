31fdb65cbe3d016de3b612b9844f44dc5a195f42
Timothy Bilton (AgR) <timothy.bilton@agresearch.co.nz>
2019-08-09 17:34:28 +1200

    Old work on cometPLot and RRDplot. Forgot I had this. Implementation to be used

--- R/RRDPlot.R
+++ R/RRDPlot.R
@@ -16,19 +16,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
-#' #' Produce a cometPlot
+#' Produce a reference ratio density (RRD) plot
 #' #'
 #' #' Some discription
 #' #'
-#' #' @usage
-#' #' RAobj$comotPlot(model="random", alpha=NULL, filename=NULL, cex=1, maxdepth=500, ...)
-#' #'
-#' #' @name $cometPlot
-#' #'
-#' #'
+#' @usage
+#' RAobj$RRDPlot(model="random", alpha=NULL, filename=NULL, cex=1, maxdepth=500, ...)
+#'
+#' @name $RRDPlot
+#'
+#'
 #'
 #' @export
-cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=500, maxSNPs=1e5, res=300, ...){
+RRDPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, maxdepth=500, maxSNPs=1e5, ...){
 
   ## Do some checks
   if(!is.matrix(ref) || GUSbase::checkVector(as.vector(ref)))
@@ -58,8 +58,10 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
     stop("Argument `maxSNPs` is invalid")
   if(GUSbase::checkVector(res, type="pos_numeric", minv = 2))
     stop("Argument `res` is invalid")
-  if(GUSbase::checkVector(cex, type="pos_numeric"))
-    stop("Argument `cex` is invalid")
+  if(GUSbase::checkVector(width, type="pos_numeric"))
+    stop("Argument `width` is invalid")
+  if(GUSbase::checkVector(height, type="pos_numeric"))
+    stop("Argument `height` is invalid")
 
   ### check if there are too many SNPs
   if(ncol(ref) > maxSNPs){
@@ -81,12 +83,14 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
   ref_sub <- factor(ref[heteCall],levels=0:maxCount,labels=0:maxCount)
   alt_sub <- factor(alt[heteCall],levels=0:maxCount,labels=0:maxCount)
   obsTab <- table(ref_sub,alt_sub)
-  ## table for cometplot
-  obsTab[upper.tri(obsTab)] <- obsTab[upper.tri(obsTab)] + t(obsTab)[upper.tri(t(obsTab))]
-
-  ## set up the count matrix
-  countMat <- matrix(nrow=maxCount+2,ncol=maxCount+2)
-  countMat[upper.tri(countMat)] <- log(obsTab[-which(lower.tri(obsTab))])
+  ## varibles for histrogram
+  counts_obs <- as.vector(obsTab)[-1]
+  counts_obs <- counts_obs/sum(counts_obs)
+  temp <- seq(maxCount+1)-1
+  tempref <- matrix(temp, nrow=maxCount+1,ncol=maxCount+1, byrow=F)
+  tempd <- matrix(temp, nrow=maxCount+1,ncol=maxCount+1, byrow=T) + tempref
+  ratio_obs <- tempref/tempd
+  ratio_obs <- as.vector(ratio_obs)[-1]
 
   rm(list=c("ref_sub","alt_sub"))
   gc()
@@ -94,22 +98,18 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
   ## Compute the expected counts
   if(is.null(gfreq)){ # if genotype frequencies are not specified
     p <- matrix(colMeans(ref/(ref+alt),na.rm=T), nrow=nrow(ref), ncol=ncol(ref), byrow=T)
-    uni_depth <- 2:maxCount
-    expCounts <- sapply(uni_depth, function(x){
+    ## data for producing density plots
+    expCounts <- sapply(1:maxCount, function(x){
       indx <- which(depth == x)
-      if(length(indx) == 0) return(numeric(floor(x/2)+1))
+      if(length(indx) == 0) return(numeric(x+1))
       else{
         pvec <- sort(unique(round(p[indx],2)))
         countp <- tabulate(round(p[indx]*100))
         countp <- countp[which(countp > 0)]
-        out <- as.matrix(sapply(1:length(pvec), function(z){
-          temp <-  countp[z]*sapply(0:x, function(y) sum(dbinom(y, x, prob=0:ploid/ploid)*dbinom(0:ploid, size=ploid, prob=(1-pvec[z]))))
-          if((x %% 2) == 0)
-            return(temp[0:(x/2)+1] + c(temp[x:(x/2+1)+1],0))
-          else
-            return(temp[0:floor(x/2)+1] + temp[x:ceiling(x/2)+1])
-        }))
-        return(rowSums(out))
+        out <- sapply(1:length(pvec), function(z){
+          return(countp[z]*sapply(0:x, function(y) sum(dbinom(y, x, prob=0:ploid/ploid)*dbinom(0:ploid, size=ploid, prob=pvec[z]))))
+        })
+        return(rev(rowSums(out)))
       }
     })
   } else{
@@ -118,59 +118,39 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
     pvec <- numeric(ncol(ref))
     for(i in 1:length(indx_p))
       pvec[indx_p[[i]]] <- i
-    expCounts <- sapply(2:maxCount, function(x){
+    expCounts <- sapply(1:maxCount, function(x){
       indx <- which(depth == x, arr.ind = T)[,2]
       if(length(indx) == 0) return(numeric(x+1))
       else{
         countp <- tabulate(pvec[indx])
         nzp <- which(countp > 0)
         countp <- countp[nzp]
-        out <- as.matrix(sapply(1:length(countp), function(z){
-          temp <- countp[z]*sapply(0:x, function(y) sum(dbinom(y, x, prob=0:ploid/ploid)*pdist[,z]))
-          if((x %% 2) == 0)
-            return(temp[0:(x/2)+1] + c(temp[x:(x/2+1)+1],0))
-          else
-            return(temp[0:floor(x/2)+1] + temp[x:ceiling(x/2)+1])
-        }))
-        return(rowSums(out))
+        out <- sapply(nzp, function(z){
+          return(countp[z]*sapply(0:x, function(y) sum(dbinom(y, x, prob=0:ploid/ploid)*pdist[,z])))
+        })
+        return(rev(rowSums(out)))
       }
     })
   }
-  expCounts <- c(0,obsTab[1,2],expCounts)
-  maxCol = length(tail(expCounts,1)[[1]])
-  expCounts_order <- c(list(cbind(unlist(lapply(expCounts, function(x) x[1])), 0:(maxCount)+1, 1)),
-                       sapply(2:maxCol, function(x)
-                         cbind(unlist(lapply(expCounts[(x*2-1):(maxCount+1)], function(y) y[x])), (x-1):(maxCount+1-x)+1,x)))
 
+  expCounts <- c(0,expCounts)
+  tempMat <- matrix(nrow=maxCount+1, ncol=maxCount+1)
+  expCounts_order <-  sapply(1:(maxCount+1), function(x)
+    cbind(unlist(lapply(expCounts[x:(maxCount+1)], function(y) y[x])), 0:(maxCount+1-x)+1,x))
   temp <- do.call("rbind",expCounts_order)
   indx_entry <- temp[,c(2:3)]
-  indx_entry[,1] <- indx_entry[,1]+1
-  countMat[indx_entry] <- log(round(temp[,1]))
+  tempMat[indx_entry] <- temp[,1]
+  counts_exp <- as.vector(tempMat)[-1]
+  counts_exp[which(is.na(counts_exp))] <- 0
+  counts_exp <- counts_exp/sum(counts_exp)
+  temp_df <- data.frame(counts=c(counts_exp,counts_obs),ratio=rep(ratio_obs,2)*ploid,
+                        method=rep(c("Expected","Observed"),rep(length(ratio_obs),2)))
 
-  ### Produce the plot
-  if(!is.null(file))
-    png(filename, width=max(maxCount*3,640)*4+sqrt(cex)*maxCount,height=max(maxCount*3,640)*4+sqrt(cex)*maxCount, res=res)
-  par(mar = c(5.1,5.1,4.1,3.1)*sqrt(cex), ...)
-  newCol <- colorRampPalette(c("red","orange","yellow","green","cyan","blue"))(200)
-  image(0:nrow(countMat),0:ncol(countMat),countMat, xlab="No. Reads (Allele 1)",ylab="No. Reads (Allele 2)",col=newCol,zlim=c(0,max(countMat,na.rm=T)),
-        cex.lab=cex,cex.axis=cex, xlim=c(0,min(max(ref,alt),maxCount) + 2), ylim=c(0,min(max(ref,alt),maxCount) + 2), mgp=c(3*sqrt(cex),sqrt(cex),0))
-  abline(0,1)
-  if(ploid > 2){
-    rat1 = 1:(ceiling(ploid/2)-1)
-    rat2 = (ploid-1):(floor(ploid/2)+1)
-    junk <- sapply(1:length(rat1), function(x) abline(0,rat1[x]/rat2[x], lty=3))
-    junk <- sapply(1:length(rat1), function(x) abline(0,rat2[x]/rat1[x], lty=3))
-  }
-  mtext("Observed",side=3,cex=cex, font=2, line=1*sqrt(cex))
-  mtext("Expected (Binomial Model)", side=4,cex=cex, font=2,line=1*sqrt(cex))
-  legend_image <- as.raster(matrix(rev(newCol), ncol = 1))
-  adj = maxCount*0.05
-  rasterImage(legend_image,xleft = maxCount-adj*2, ybottom = adj, ytop = 5*adj, xright = maxCount-adj )
-  text(x = maxCount-adj*2, y = adj + seq(0,4*adj,length.out=6), labels = format(exp(seq(0,max(countMat,na.rm=T),length.out=6)),digits=1),cex=cex, pos=2)
-  if(!is.null(file))
-    dev.off()
-
-  return(invisible())
+  ## Produce RRD plot
+  pp <- ggplot2::ggplot(temp_df, ggplot2::aes(x=ratio, fill=method)) +
+    ggplot2::geom_density(ggplot2::aes(weight=counts), alpha=0.2) + ggplot2::theme_bw() +
+    ggplot2::xlab("Dosage of reference allele") + ggplot2::theme(legend.title = ggplot2::element_blank(), legend.position="top")
+  if(is.null(file)) print(pp)
+  else  ggplot2::ggsave(filename=paste0(file,"_RRD.png"), plot=pp,...)
+  return(invisible(NULL))
 }
-
-
