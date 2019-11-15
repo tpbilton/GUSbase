83fae0343d32073994493d74a54631055cf72167
Timothy P. Bilton (Home) <tpbilton@gmail.com>
2019-11-15 15:11:37 +1300

    Changes to Comet plot. Added alternative scaling

--- R/cometPlot.R
+++ R/cometPlot.R
@@ -29,7 +29,7 @@
 #'
 #' @export
 cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=500, maxSNPs=1e5, res=300,
-                      difference=F, scaled=T, power=2, ...){
+                      difference=F, scaled="exp", power=2, ...){
 
   ## Do some checks
   if(!is.matrix(ref) || GUSbase::checkVector(as.vector(ref)))
@@ -261,7 +261,84 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
     expMat[indx_entry] <- temp[,1]
 
     ## Produce the plot
-    if(!scaled){
+    if(scaled == "counts"){
+      newCol <- colorRampPalette(c("red","orange","yellow","white","cyan","#0080FF","blue"))(200)
+      maxdepth <- min(max(ref,alt),maxdepth)
+      newMat <- (obsTab-expMat)
+      tab_rd = c(0,tabulate(ref + alt, nbins = max(ref+alt)))
+      rd <- matrix(0:maxdepth, nrow=maxdepth+1, ncol=maxdepth+1) + matrix(0:maxdepth, nrow=maxdepth+1, ncol=maxdepth+1, byrow=T)
+      rd_mat <- matrix(tab_rd[rd+1], nrow=nrow(rd), ncol=ncol(rd))
+      newMat2 <- newMat[0:maxdepth+1,0:maxdepth+1]
+      newMat2 <- newMat2/rd_mat
+      indx <- which(newMat2<0)
+      newMat2[indx] <- -(abs(newMat2[indx]))^(1/power)
+      indx <- which(newMat2>=0)
+      newMat2[indx] <- (newMat2[indx])^(1/power)
+      ran <- max(abs(newMat2),na.rm=T)
+
+      if(!is.null(file))
+        png(filename, width=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth,height=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth, res=res)
+      par(mar = c(5.1,5.1,1.1,1.1)*sqrt(cex), ...)
+      image(0:maxdepth,0:maxdepth,t(newMat2),  zlim=c(-ran,ran), col=newCol, cex.lab=cex,cex.axis=cex,
+            xaxt = "n", yaxt="n", ylab="Reference Allele Count", xlab="Alternate Allele Count",mgp=c(3*sqrt(cex),sqrt(cex),0))
+      ticks <- seq(par()$xaxp[1],par()$xaxp[2],par()$xaxp[2]/par()$xaxp[3])
+      graphics::axis(side = 1, at = ticks+0,labels = ticks, cex.axis=cex)
+      graphics::axis(side = 2, at = ticks+0,labels = ticks, cex.axis=cex)
+      graphics::grid()
+      graphics::box()
+
+      if(ploid > 2){
+        rat1 = 1:(ploid-1)
+        rat2 = rev(rat1)
+        junk <- base::sapply(1:length(rat1), function(x) graphics::abline(0,rat1[x]/rat2[x], lty=3, cex=cex))
+      } else if(ploid == 2) graphics::abline(0,1, lty=3, cex=cex)
+
+      legend_image <- as.raster(matrix(rev(newCol), ncol = 1))
+      adj = min(max(ref,alt),maxdepth)*0.05
+      rasterImage(legend_image,xleft = min(max(ref,alt),maxdepth)-adj*2, ybottom = adj, ytop = 6*adj, xright = min(max(ref,alt),maxdepth)-adj )
+      lines(x = c(rep(min(max(ref,alt),maxdepth)-adj*2,2),rep(min(max(ref,alt),maxdepth)-adj,2),min(max(ref,alt),maxdepth)-adj*2), y=c(adj,rep(6*adj,2),adj,adj),cex=cex)
+      num <- unique(c(-seq(1,0, length.out=4)^(power),seq(0,1,length.out=4)^(power)))
+      text(x = min(max(ref,alt),maxdepth)-adj*2-1, y = adj + seq(0,5*adj,length.out=7), labels = format(num,digits=2, scientific=F),cex=cex, pos=2)
+      junk <- sapply(adj + seq(0,5*adj,length.out=7), function(y) lines(min(max(ref,alt),maxdepth) - adj*2 + c(0,-1), y=rep(y,2), cex=cex))
+      if(!is.null(file))
+        dev.off()
+    } else if(scaled == "exp"){
+      newCol <- colorRampPalette(c("red","orange","yellow","white","cyan","#0080FF","blue"))(500)
+      maxdepth <- min(max(ref,alt),maxdepth)
+      newMat <- (obsTab-expMat)
+      newMat2 <- newMat[0:maxdepth+1,0:maxdepth+1]
+      newMat2 <- newMat2/(expMat[0:maxdepth+1,0:maxdepth+1])
+      newMat2[which(newMat2< -20)] <- -20
+      newMat2[which(newMat2> 20)] <- 20
+      ran <- max(abs(newMat2),na.rm=T)
+
+      if(!is.null(file))
+        png(filename, width=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth,height=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth, res=res)
+      par(mar = c(5.1,5.1,1.1,1.1)*sqrt(cex), ...)
+      image(0:maxdepth,0:maxdepth,t(newMat2),  zlim=c(-ran,ran), col=newCol, cex.lab=cex,cex.axis=cex,
+            xaxt = "n", yaxt="n", ylab="Reference Allele Count", xlab="Alternate Allele Count",mgp=c(3*sqrt(cex),sqrt(cex),0))
+      ticks <- seq(par()$xaxp[1],par()$xaxp[2],par()$xaxp[2]/par()$xaxp[3])
+      graphics::axis(side = 1, at = ticks+0,labels = ticks, cex.axis=cex)
+      graphics::axis(side = 2, at = ticks+0,labels = ticks, cex.axis=cex)
+      graphics::grid()
+      graphics::box()
+
+      if(ploid > 2){
+        rat1 = 1:(ploid-1)
+        rat2 = rev(rat1)
+        junk <- base::sapply(1:length(rat1), function(x) graphics::abline(0,rat1[x]/rat2[x], lty=3, cex=cex))
+      } else if(ploid == 2) graphics::abline(0,1, lty=3, cex=cex)
+
+      legend_image <- as.raster(matrix(rev(newCol), ncol = 1))
+      adj = min(max(ref,alt),maxdepth)*0.05
+      rasterImage(legend_image,xleft = min(max(ref,alt),maxdepth)-adj*2, ybottom = adj, ytop = 6*adj, xright = min(max(ref,alt),maxdepth)-adj )
+      lines(x = c(rep(min(max(ref,alt),maxdepth)-adj*2,2),rep(min(max(ref,alt),maxdepth)-adj,2),min(max(ref,alt),maxdepth)-adj*2), y=c(adj,rep(6*adj,2),adj,adj),cex=cex)
+      num <- unique(c(-seq(20,0, length.out=4),seq(0,20,length.out=4)))
+      text(x = min(max(ref,alt),maxdepth)-adj*2-1, y = adj + seq(0,5*adj,length.out=7), labels = format(num,digits=2, scientific=F),cex=cex, pos=2)
+      junk <- sapply(adj + seq(0,5*adj,length.out=7), function(y) lines(min(max(ref,alt),maxdepth) - adj*2 + c(0,-1), y=rep(y,2), cex=cex))
+      if(!is.null(file))
+        dev.off()
+    } else{
       newMat <- (obsTab-expMat)
       maxDiff <- max(abs(newMat), na.rm=T)
       indx <- which(newMat > 0)
@@ -296,47 +373,6 @@ cometPlot <- function(ref, alt, ploid=2, gfreq=NULL, file=NULL, cex=1, maxdepth=
       lines(x = c(rep(min(max(ref,alt),maxdepth)-adj*2,2),rep(min(max(ref,alt),maxdepth)-adj,2),min(max(ref,alt),maxdepth)-adj*2), y=c(adj,rep(6*adj,2),adj,adj),cex=cex)
       num <- unique(c(-(exp(seq(max(abs(newMat),na.rm=T),0, length.out=4))-1),exp(seq(0,max(abs(newMat), na.rm=T),length.out=4))-1))
       text(x = min(max(ref,alt),maxdepth)-adj*2-1, y = adj + seq(0,5*adj,length.out=7), labels = format(num,digits=1, scientific=F),cex=cex, pos=2)
-      junk <- sapply(adj + seq(0,5*adj,length.out=7), function(y) lines(min(max(ref,alt),maxdepth) - adj*2 + c(0,-1), y=rep(y,2), cex=cex))
-      if(!is.null(file))
-        dev.off()
-    } else{
-      newCol <- colorRampPalette(c("red","orange","yellow","white","cyan","#0080FF","blue"))(200)
-      maxdepth <- min(max(ref,alt),maxdepth)
-      newMat <- (obsTab-expMat)
-      tab_rd = c(0,tabulate(ref + alt, nbins = max(ref+alt)))
-      rd <- matrix(0:maxdepth, nrow=maxdepth+1, ncol=maxdepth+1) + matrix(0:maxdepth, nrow=maxdepth+1, ncol=maxdepth+1, byrow=T)
-      rd_mat <- matrix(tab_rd[rd+1], nrow=nrow(rd), ncol=ncol(rd))
-      newMat2 <- newMat[0:maxdepth+1,0:maxdepth+1]
-      newMat2 <- newMat2/rd_mat
-      indx <- which(newMat2<0)
-      newMat2[indx] <- -(abs(newMat2[indx]))^(1/power)
-      indx <- which(newMat2>=0)
-      newMat2[indx] <- (newMat2[indx])^(1/power)
-      ran <- max(abs(newMat2),na.rm=T)
-
-      if(!is.null(file))
-        png(filename, width=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth,height=max(maxdepth*3,640)*4+sqrt(cex)*maxdepth, res=res)
-      par(mar = c(5.1,5.1,1.1,1.1)*sqrt(cex), ...)
-      image(0:maxdepth,0:maxdepth,t(newMat2),  zlim=c(-ran,ran), col=newCol, cex.lab=cex,cex.axis=cex,
-            xaxt = "n", yaxt="n", ylab="Reference Allele Vount", xlab="Alternate Allele Count",mgp=c(3*sqrt(cex),sqrt(cex),0))
-      ticks <- seq(par()$xaxp[1],par()$xaxp[2],par()$xaxp[2]/par()$xaxp[3])
-      graphics::axis(side = 1, at = ticks+0,labels = ticks, cex.axis=cex)
-      graphics::axis(side = 2, at = ticks+0,labels = ticks, cex.axis=cex)
-      graphics::grid()
-      graphics::box()
-
-      if(ploid > 2){
-        rat1 = 1:(ploid-1)
-        rat2 = rev(rat1)
-        junk <- base::sapply(1:length(rat1), function(x) graphics::abline(0,rat1[x]/rat2[x], lty=3, cex=cex))
-      } else if(ploid == 2) graphics::abline(0,1, lty=3, cex=cex)
-
-      legend_image <- as.raster(matrix(rev(newCol), ncol = 1))
-      adj = min(max(ref,alt),maxdepth)*0.05
-      rasterImage(legend_image,xleft = min(max(ref,alt),maxdepth)-adj*2, ybottom = adj, ytop = 6*adj, xright = min(max(ref,alt),maxdepth)-adj )
-      lines(x = c(rep(min(max(ref,alt),maxdepth)-adj*2,2),rep(min(max(ref,alt),maxdepth)-adj,2),min(max(ref,alt),maxdepth)-adj*2), y=c(adj,rep(6*adj,2),adj,adj),cex=cex)
-      num <- unique(c(-seq(1,0, length.out=4)^(power),seq(0,1,length.out=4)^(power)))
-      text(x = min(max(ref,alt),maxdepth)-adj*2-1, y = adj + seq(0,5*adj,length.out=7), labels = format(num,digits=2, scientific=F),cex=cex, pos=2)
       junk <- sapply(adj + seq(0,5*adj,length.out=7), function(y) lines(min(max(ref,alt),maxdepth) - adj*2 + c(0,-1), y=rep(y,2), cex=cex))
       if(!is.null(file))
         dev.off()
