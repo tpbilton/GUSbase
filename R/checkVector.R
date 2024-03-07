4ab9b35b839a01e03a1b28841e7c8b931c4e1e75
Timothy Bilton <tpbilton@users.noreply.github.com>
2024-03-07 16:48:18 +1300

    Update checkVector.R
    Fixed some bugs

--- R/checkVector.R
+++ R/checkVector.R
@@ -6,7 +6,7 @@ checkVector <- function(x, type="pos_integer", minv=0, maxv=NULL, equal=TRUE){
   if(type=="pos_integer" & !is.null(maxv))
     return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || !all(x == round(x)) || any(x < minv) || any(x > maxv))
   else if(type=="pos_integer" & is.null(maxv))
-    return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || !all(x == round(x)) || any(x < minv) || is.infinite(x))
+    return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || !all(x == round(x)) || any(x < minv) || any(is.infinite(x)))
   else if(type=="pos_numeric" & !is.null(maxv)){
     if(equal)
       return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || any(x <= minv) || any(x >= maxv))
@@ -15,9 +15,9 @@ checkVector <- function(x, type="pos_integer", minv=0, maxv=NULL, equal=TRUE){
   }
   else if(type=="pos_numeric" & is.null(maxv)){
     if(equal)
-      return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || any(x <= minv) || is.infinite(x))
+      return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || any(x <= minv) || any(is.infinite(x)))
     else
-      return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || any(x < minv) || is.infinite(x))
+      return(!is.vector(x) || !is.numeric(x) || any(is.na(x)) || any(x < minv) || any(is.infinite(x)))
   } else if(type == "one_logical")
     return(!is.logical(x) || any(is.na(x)) || length(x) != 1)
 }
